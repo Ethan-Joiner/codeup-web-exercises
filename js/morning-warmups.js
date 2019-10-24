523f5d0385e51ce016cbeba579074e3bf2b5cdb8
Ethan Joiner <ethan.joiner@gmail.com>
2019-10-24 09:14:49 -0500

    thurs warmup

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -146,3 +146,18 @@ function createPersonObject(x, y) {
 
 console.log(createPersonObject("ethan", 29));
 
+function fizzbuzz() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 ===0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzbuzz();
