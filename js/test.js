0df4f0e5051a7fb6a7031b71e82fe3d02694e3e7
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-04 10:23:02 -0600

    idk

--- js/test.js
+++ js/test.js
@@ -6,10 +6,9 @@ $(document).ready(function() {
     $('div').click(function() {
         $(this).css('position', 'absolute');
         $(this).animate({ left: "+=200" }, 2000 ).queue(function() {
-            $(this).animate({ top: "+=400" }, 2000 );
-            // $(this).css('color', 'blue')
+            $('div').animate({ top: "+=400" }, 2000 );
+            $(this).css('color', 'blue')
         })
     })
-
 
 });
