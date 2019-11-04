57da95e354f28747dab3a6781be79a350e63d5b7
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-04 11:22:21 -0600

    events done

--- js/test.js
+++ js/test.js
@@ -3,12 +3,42 @@
 $(document).ready(function() {
 
 
-    $('div').click(function() {
-        $(this).css('position', 'absolute');
-        $(this).animate({ left: "+=200" }, 2000 ).queue(function() {
-            $('div').animate({ top: "+=400" }, 2000 );
-            $(this).css('color', 'blue')
-        })
-    })
+    // $('div').click(function() {
+    //     $(this).css('position', 'absolute');
+    //     $(this)
+    //         .animate({ left: "+=200" }, 2000 )
+    //         .animate({ top: "+=400" }, 2000 )
+    //         $(this).css('color', 'blue')
+    //     })
 
 });
+
+// var introString = "Each event is preceded by Prophecy.
+//                     But without the hero,
+//                         there is no Event.
+//                     -Zurin Arctus | the Underking
+//
+//                     In the waning years of the
+//                     Third Era of Tamriel,
+//                         a prisoner born on a certain day
+//                     to uncertain parents was sent
+//                     under guard, without explanation, to Morrowind,
+//                         ignorant of the role he was
+//                     to play in that nation'\s history…
+//
+//                     (female voice)
+//                     They have taken you from the Imperial City'\s prison,
+//                     first by carriage and now by boat,
+//                         to the east to Morrowind.
+//                         Fear not, for I am watchful,
+//                         you have been chosen.
+//
+//                     (male voice)
+//                     Wake up, we are here.
+//                     Why are you shaking?
+//                         Are you okay? Wakeup!";
+
+var test = "This\
+    is a test.";
+
+console.log(test)
