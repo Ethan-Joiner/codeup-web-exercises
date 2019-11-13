8ce203cc552f5534e40601f3dfd6253633d6d6f2
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-13 11:14:27 -0600

    done

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -44,3 +44,43 @@ console.log(three);
 let email = users.map(user => user.email);
 console.log(email);
 
+let totalYears = users.reduce(((a,b) => a + b.yearsOfExperience), 0);
+console.log(totalYears);
+
+let average = totalYears / users.length;
+console.log(average);
+
+let longestEmail = users.reduce(((a,b) => {
+    if (a.length < b.email.length) {
+        a = b.email;
+    }
+    return a
+}),' ');
+console.log(longestEmail);
+
+// let nameString = users.reduce(((a,b) => {
+//     a.push(b.name);
+//     console.log(a);
+//     console.log(b.name);
+//     // return `${a} ${b.name},`
+// }),[]);
+// console.log(nameString);
+//
+// let names = users.map(user => user.name);
+// console.log(names);
+
+let instructors = users.reduce(((str, user) => {
+    return `${str},${user.name}`
+}), "Your instructors are: ") + ".";
+console.log(instructors);
+
+let listOfLanguages = Array.from(users.reduce((accumulator, user) => {
+    user.languages.forEach(language => accumulator.add(language));
+    return accumulator
+}, new Set()));
+console.log(listOfLanguages);
+
+// const listOfLanguages = Array.from(users.reduce((accumulator,user) => {
+//     user.languages.forEach(language => accumulator.add(language));
+//     return accumulator;
+// }, new Set()));
